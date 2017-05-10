78b67e4b1a000c43a8384873e6517fbe3c4db00e
leamendoza97 <lea.mendoza97@gmail.com>
2017-05-10 19:50:38 -0300

    primera subidita

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> // Inluye todas las librerias standar de C++
+
+using namespace std;
+
+int main(){
+	#ifdef ONLINE_JUDGE
+		ios_base::sync_with_stdio(0); cin.tie(0);
+	#endif
+
+	// TODO : codigo aca
+
+	return 0;
+}
